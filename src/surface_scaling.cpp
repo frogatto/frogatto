fbb054e1f327637a8fb5935e262f79037cbbb23c
davewx7 <davewx7@c997a996-8b50-0410-b275-33e14bc6d6d6>
2008-10-15 21:17:48 +0000

    added missing file

---
+++ src/surface_scaling.cpp
@@ -0,0 +1,24 @@
+#include <inttypes.h>
+
+#include "surface.hpp"
+
+namespace graphics {
+
+surface scale_surface(surface input) {
+	surface result(surface::create(input->w*2, input->h*2));
+
+	const uint32_t* in = reinterpret_cast<const uint32_t*>(input->pixels);
+	uint32_t* out = reinterpret_cast<uint32_t*>(result->pixels);
+	for(int y = 0; y != input->h; ++y) {
+		for(int x = 0; x != input->w; ++x) {
+			out[(y*2)*result->w + x*2] = in[y*input->w + x];
+			out[(y*2 + 1)*result->w + x*2] = in[y*input->w + x];
+			out[(y*2 + 1)*result->w + x*2 + 1] = in[y*input->w + x];
+			out[(y*2)*result->w + x*2 + 1] = in[y*input->w + x];
+		}
+	}
+
+	return result;
+}
+
+}
