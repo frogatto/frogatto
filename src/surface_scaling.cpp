11738bb048356522ec9b9ef2c08a75c2a21f11d3
kettering.richard <kettering.richard@c997a996-8b50-0410-b275-33e14bc6d6d6>
2008-10-20 15:30:34 +0000

    Added a comment regarding eagle mode interpolation for later use.

--- src/surface_scaling.cpp
+++ src/surface_scaling.cpp
@@ -11,10 +11,17 @@ surface scale_surface(surface input) {
 	uint32_t* out = reinterpret_cast<uint32_t*>(result->pixels);
 	for(int y = 0; y != input->h; ++y) {
 		for(int x = 0; x != input->w; ++x) {
+		  //do nearest neighbor interpolation
 			out[(y*2)*result->w + x*2] = in[y*input->w + x];
 			out[(y*2 + 1)*result->w + x*2] = in[y*input->w + x];
 			out[(y*2 + 1)*result->w + x*2 + 1] = in[y*input->w + x];
 			out[(y*2)*result->w + x*2 + 1] = in[y*input->w + x];
+			
+			/*if ((y > 0) && (y < input->h) && (x > 0) && (x < input->w)){
+			//do additional eagle interpolation
+						//code goes here
+			
+			}*/
 		}
 	}
 
