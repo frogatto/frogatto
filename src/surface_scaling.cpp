07bc12fdf019a02d9cb0078b7944008c19091c25
kettering.richard <kettering.richard@c997a996-8b50-0410-b275-33e14bc6d6d6>
2008-10-22 22:35:50 +0000

    Added the Eagle scaling algorithm.  Commented out till I have a chance to actually test it.

--- src/surface_scaling.cpp
+++ src/surface_scaling.cpp
@@ -19,8 +19,35 @@ surface scale_surface(surface input) {
 			
 			/*if ((y > 0) && (y < input->h) && (x > 0) && (x < input->w)){
 			//do additional eagle interpolation
-						//code goes here
-			
+			//   first:        |Then 
+			//   . . . --\ CC  |S T U  --\ 1 2
+			//   . C . --/ CC  |V C W  --/ 3 4
+			//   . . .         |X Y Z
+			//                 | IF V==S==T => 1=S
+			//                 | IF T==U==W => 2=U
+			//                 | IF V==X==Y => 3=X
+			//                 | IF W==Z==Y => 4=Z
+						
+						if ( (in[y*input->w + x - input->w - 1] == in[y*input->w + x - input->w]) && 
+									(in[y*input->w + x - input->w - 1] == in[y*input->w + x - 1]) ) {
+						  //IF V==S==T => 1=S
+								out[(y*2)*result->w + x*2] = in[y*input->w + x + 1];
+						}
+						if ( (in[y*input->w + x - input->w + 1] == in[y*input->w + x - input->w]) && 
+									(in[y*input->w + x - input->w + 1] == in[y*input->w + x + 1]) ) {
+						  //IF T==U==W => 2=U
+								out[(y*2)*result->w + x*2 + 1] = in[y*input->w + x + 1];
+						}
+						if ( (in[y*input->w + x + input->w - 1] == in[y*input->w + x + input->w]) && 
+									(in[y*input->w + x + input->w - 1] == in[y*input->w + x - 1]) ) {
+						  //IF V==X==Y => 3=X
+								out[(y*2)*result->w + x*2 + 1] = in[y*input->w + x + 1];
+						}
+						if ( (in[y*input->w + x + input->w + 1] == in[y*input->w + x + input->w]) && 
+									(in[y*input->w + x + input->w + 1] == in[y*input->w + x + 1]) ) {
+						  //IF W==Z==Y => 4=Z
+								out[(y*2)*result->w + x*2 + 1] = in[y*input->w + x + 1];
+						}
 			}*/
 		}
 	}
