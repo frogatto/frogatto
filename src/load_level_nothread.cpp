e56de374bba42d81d2e1ae2e1808a57538fcb77e
davewx7 <davewx7@c997a996-8b50-0410-b275-33e14bc6d6d6>
2008-12-03 08:00:13 +0000

    fixed loading levels with no threading

--- src/load_level_nothread.cpp
+++ src/load_level_nothread.cpp
@@ -1,3 +1,4 @@
+#include "filesystem.hpp"
 #include "level.hpp"
 #include "load_level.hpp"
 
@@ -18,4 +19,18 @@ level* load_level(const std::string& lvl)
 	level* res = new level(lvl);
 	res->finish_loading();
 	return res;
+}
+
+namespace {
+bool not_cfg_file(const std::string& filename) {
+	return filename.size() < 4 || !std::equal(filename.end() - 4, filename.end(), ".cfg");
+}
+}
+
+std::vector<std::string> get_known_levels()
+{
+	std::vector<std::string> files;
+	sys::get_files_in_dir("data/level/", &files);
+	files.erase(std::remove_if(files.begin(), files.end(), not_cfg_file), files.end());
+	return files;
 }
