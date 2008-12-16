2d235e00603c3e822b9312461ff16de106b8176e
kettering.richard <kettering.richard@c997a996-8b50-0410-b275-33e14bc6d6d6>
2008-12-16 03:31:57 +0000

    Corrected a typo in the assert macros.

--- src/asserts.hpp
+++ src/asserts.hpp
@@ -9,17 +9,17 @@
 
 #define assert_ne(a,b) if((a) == (b)) { std::cerr << __FILE__ << ":" << __LINE__ << " ASSERT NE FAILED: " << #a << " == " << #b << ": " << (a) << " == " << (b) << "\n"; abort(); }
 
-#define assert_ge(a,b) if((a) < (b)) { std::cerr << __FILE__ << ":" << __LINE__ << " ASSERT GE FAILED: " << #a << " == " << #b << ": " << (a) << " == " << (b) << "\n"; abort(); }
+#define assert_ge(a,b) if((a) < (b)) { std::cerr << __FILE__ << ":" << __LINE__ << " ASSERT GE FAILED: " << #a << " < " << #b << ": " << (a) << " < " << (b) << "\n"; abort(); }
 
-#define assert_le(a,b) if((a) > (b)) { std::cerr << __FILE__ << ":" << __LINE__ << " ASSERT LE FAILED: " << #a << " == " << #b << ": " << (a) << " == " << (b) << "\n"; abort(); }
+#define assert_le(a,b) if((a) > (b)) { std::cerr << __FILE__ << ":" << __LINE__ << " ASSERT LE FAILED: " << #a << " > " << #b << ": " << (a) << " > " << (b) << "\n"; abort(); }
 
-#define assert_gt(a,b) if((a) <= (b)) { std::cerr << __FILE__ << ":" << __LINE__ << " ASSERT GT FAILED: " << #a << " == " << #b << ": " << (a) << " == " << (b) << "\n"; abort(); }
+#define assert_gt(a,b) if((a) <= (b)) { std::cerr << __FILE__ << ":" << __LINE__ << " ASSERT GT FAILED: " << #a << " <= " << #b << ": " << (a) << " <= " << (b) << "\n"; abort(); }
 
-#define assert_lt(a,b) if((a) >= (b)) { std::cerr << __FILE__ << ":" << __LINE__ << " ASSERT LT FAILED: " << #a << " == " << #b << ": " << (a) << " == " << (b) << "\n"; abort(); }
+#define assert_lt(a,b) if((a) >= (b)) { std::cerr << __FILE__ << ":" << __LINE__ << " ASSERT LT FAILED: " << #a << " >= " << #b << ": " << (a) << " >= " << (b) << "\n"; abort(); }
 
 //for custom logging.  Example usage:
 //assert_log(x != y, "x not equal to y. Value of x: " << x << ", y: " << y);
-#define assert_log(a,b) if( !(a) ) { std::cerr << __FILE__ << ":" << __LINE__ << " ASSSERTION FAILED: " << (b) << "\n"; abort(); }
+#define assert_log(a,b) if( !(a) ) { std::cerr << __FILE__ << ":" << __LINE__ << " ASSSERTION FAILED: " << b << "\n"; abort(); }
 
 
 #endif
