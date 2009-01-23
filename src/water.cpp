b75c21fd6d9b596d67ca9e5e553609256a29b802
kettering.richard <kettering.richard@c997a996-8b50-0410-b275-33e14bc6d6d6>
2009-01-23 07:01:58 +0000

    Corrected the water colors.

--- src/water.cpp
+++ src/water.cpp
@@ -19,7 +19,6 @@ water::water(wml::const_node_ptr water_node) : level_(wml::get_int(water_node, "
 		pos.zorder = wml::get_int(layer_node, "zorder");
 		pos.offset = wml::get_int(layer_node, "offset");
 		pos.color = string_to_color(layer_node->attr("color"));
-		std::cerr << "MY_COLOR: " << layer_node->attr("color") << "\n";
 		
 		positions_.push_back(pos);
 	}
@@ -36,7 +35,7 @@ wml::node_ptr water::write() const
 		node->set_attr("color", buf);
 		node->set_attr("zorder", formatter() << pos.zorder);
 		node->set_attr("offset", formatter() << pos.offset);
-
+		
 		result->add_child(node);
 	}
 	return result;
@@ -47,22 +46,22 @@ void water::draw(int begin_layer, int end_layer, int x, int y, int w, int h) con
 	if(begin_layer < min_zorder()) {
 		begin_layer = min_zorder();
 	}
-
+	
 	if(end_layer > max_zorder()) {
 		end_layer = max_zorder();
 	}
-
+	
 	if(begin_layer >= end_layer) {
 		return;
 	}
-
+	
 	const int offset1 = get_offset(begin_layer);
 	const int offset2 = get_offset(end_layer);
-
+	
 	const SDL_Rect r = {x, level_ + offset1, w, offset2 - offset1};
 	const SDL_Color water_color = get_color(offset1);
-	graphics::draw_rect(r, water_color, 128);
-
+	graphics::draw_rect(r, water_color, 200);
+	
 	// draw the water edge and the deep, bottom-half-of-the-screen-filling underwater layer
 	const int surface = level_ + offset2;
 	if(end_layer == max_zorder() && y + w >= surface) {
@@ -78,37 +77,37 @@ void water::draw(int begin_layer, int end_layer, int x, int y, int w, int h) con
 SDL_Color water::get_color(int offset) const
 {
 	int index = 0;
-	 for(; index != positions_.size(); ++index) {
-		 //loop through till we find the first one with an offset greater than what we're looking at, and return its color
-		 if (positions_[index].offset > offset){
-			 return positions_[index].color;
-		 }
-	 }
+	for(; index != positions_.size(); ++index) {
+		//loop through till we find the first one with an offset greater than what we're looking at, and return its color
+		if (positions_[index].offset > offset){
+			return positions_[index].color;
+		}
+	}
 	return graphics::color_blue();
 }
 
 int water::get_offset(int zorder) const
 {
 	/*int index = 0;
-	for(; index != positions_.size(); ++index) {
-		if(zorder == positions_[index].zorder) {
-			return positions_[index].offset;
-		}
-
-		if(zorder < positions_[index].zorder) {
-			if(index == 0) {
-				return BadOffset;
-			}
-
-			const float r1 = zorder - positions_[index-1].zorder;
-			const float r2 = positions_[index].zorder - zorder;
-			const float offset = (positions_[index-1].offset*r2 + positions_[index].offset*r1)/(r1+r2);
-			return static_cast<int>(offset);
-		}
-	}
-
-	return BadOffset;*/
-
+	 for(; index != positions_.size(); ++index) {
+	 if(zorder == positions_[index].zorder) {
+	 return positions_[index].offset;
+	 }
+	 
+	 if(zorder < positions_[index].zorder) {
+	 if(index == 0) {
+	 return BadOffset;
+	 }
+	 
+	 const float r1 = zorder - positions_[index-1].zorder;
+	 const float r2 = positions_[index].zorder - zorder;
+	 const float offset = (positions_[index-1].offset*r2 + positions_[index].offset*r1)/(r1+r2);
+	 return static_cast<int>(offset);
+	 }
+	 }
+	 
+	 return BadOffset;*/
+	
 	
 	//normalize the zorder value to a scale from 0 <-> 1, then linearly transform that to an offset
 	float zorder_scale = max_zorder() - min_zorder();
@@ -123,7 +122,7 @@ int water::min_zorder() const
 	if(positions_.empty()) {
 		return BadOffset;
 	}
-
+	
 	return positions_.front().zorder;
 }
 
@@ -132,7 +131,7 @@ int water::max_zorder() const
 	if(positions_.empty()) {
 		return BadOffset;
 	}
-
+	
 	return positions_.back().zorder;
 }
 
@@ -148,6 +147,12 @@ int water::min_offset() const
 int water::max_offset() const
 {
 	if(positions_.empty()) {
+		return BadOffset;
+	}
+	
+	return positions_.back().offset;
+}
+()) {
 		return BadOffset;
 	}
 
