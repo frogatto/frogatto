ace49eef84e8784912e87be9db5c43440a8008e7
kettering.richard <kettering.richard@c997a996-8b50-0410-b275-33e14bc6d6d6>
2009-02-25 19:49:31 +0000

    Fixed modulo divide-by-zero error in play_sound that wasn't showing up on PPC.

--- src/frame.cpp
+++ src/frame.cpp
@@ -86,8 +86,8 @@ frame::frame(wml::const_node_ptr node)
 
 void frame::play_sound(const void* object) const
 {
-	int randomNum = rand()%sounds_.size();  //like a 1d-size die
 	if (sounds_.empty() == false){
+		int randomNum = rand()%sounds_.size();  //like a 1d-size die
 		if(sounds_[randomNum].empty() == false) {
 			std::cerr << "PLAY SOUND: '" << sounds_[randomNum] << "'\n";
 			sound::play(sounds_[randomNum], object);
