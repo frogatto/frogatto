b3984b9e3145ac3c4f00df1ff29ce29c6e8a4cd4
kettering.richard <kettering.richard@c997a996-8b50-0410-b275-33e14bc6d6d6>
2009-04-23 01:03:06 +0000

    Added a mostly-complete color class.

--- src/color_utils.cpp
+++ src/color_utils.cpp
@@ -1,4 +1,8 @@
 #include "color_utils.hpp"
+#include <string>
+#include <vector>
+#include "string_utils.hpp"
+
 
 SDL_Color string_to_color(const std::string& str)
 {
@@ -11,3 +15,51 @@ SDL_Color string_to_color(const std::string& str)
 	}
 	return res;
 }
+
+using namespace graphics;
+
+color::color( uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+{
+	c_.rgba[0] = r;
+	c_.rgba[1] = g;
+	c_.rgba[2] = b;
+	c_.rgba[3] = a;
+	
+}
+
+color::color( uint32_t rgba)
+{
+	c_.value = rgba;
+}
+
+color::color( const std::string& str)
+{
+	std::vector<std::string> components;
+	components = util::split(str);
+	
+	if( components.size() == 4){
+		int i=0;
+		for( std::vector<std::string>::iterator itor = components.begin();
+			itor != components.end(); ++itor)
+		{
+			c_.rgba[i] = atoi(itor->c_str());
+		}
+	}
+}
+
+
+variant color::get_value(const std::string& key) const
+{
+	if(key == "r"){
+		return variant(r());
+	} else if(key == "g"){
+		return variant(g());
+	} else if(key == "b"){
+		return variant(b());
+	} else if(key == "a"){
+		return variant(a());
+	} else {
+		return variant();
+	}
+}
+
--- src/color_utils.hpp
+++ src/color_utils.hpp
@@ -4,7 +4,39 @@
 #include "SDL.h"
 
 #include <string>
+#include <vector>
+#include "formula_callable.hpp"
 
 SDL_Color string_to_color(const std::string& str);
 
+
+namespace graphics {
+class color : public game_logic::formula_callable
+{
+	public:
+	uint8_t r() const {return c_.rgba[0]; }
+	uint8_t g() const {return c_.rgba[1]; }
+	uint8_t b() const {return c_.rgba[2]; }
+	uint8_t a() const {return c_.rgba[3]; }
+	uint32_t rgba() const {return c_.value; }
+	//TODO: color to return string version
+	
+	color( uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+	color( uint32_t rgba);
+	color( const std::string& str);
+	
+	variant color::get_value(const std::string& key) const;
+
+	
+	private:
+		union PixelUnion {
+			uint32_t value;
+			uint8_t rgba[4];
+		};
+
+		PixelUnion c_;
+		
+};
+	
+}
 #endif
