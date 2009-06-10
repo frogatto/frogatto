f7293b29bcae8ec6887c92f754842a37e09e1438
davewx7 <davewx7@c997a996-8b50-0410-b275-33e14bc6d6d6>
2009-06-10 04:57:32 +0000

    added rectangle current regions

--- src/current_generator.hpp
+++ src/current_generator.hpp
@@ -4,6 +4,7 @@
 #include <boost/intrusive_ptr.hpp>
 
 #include "formula_callable.hpp"
+#include "geometry.hpp"
 #include "wml_node_fwd.hpp"
 
 typedef boost::intrusive_ptr<class current_generator> current_generator_ptr;
@@ -33,6 +34,19 @@ public:
 private:
 	int intensity_;
 	int radius_;
+};
+
+class rect_current_generator : public current_generator
+{
+public:
+	rect_current_generator(const rect& r, int xdir, int ydir);
+	explicit rect_current_generator(wml::const_node_ptr node);
+	virtual void generate(int center_x, int center_y, int target_x, int target_y, int* velocity_x, int* velocity_y);
+
+	virtual wml::node_ptr write() const;
+private:
+	rect rect_;
+	int xdir_, ydir_;
 };
 
 #endif
