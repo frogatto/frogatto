3e895c2a1a8b1962031deaca6ae0662d1908d518
davewx7 <davewx7@c997a996-8b50-0410-b275-33e14bc6d6d6>
2009-10-24 06:06:55 +0000

    made frogatto bounce off springy objects again

--- src/custom_object.cpp
+++ src/custom_object.cpp
@@ -598,6 +598,8 @@ void custom_object::process(level& lvl)
 		//we are standing on a new object. Adjust our velocity relative to
 		//the object we're standing on
 		velocity_x_ -= stand_info.collide_with->last_move_x()*100;
+
+		stand_info.collide_with->handle_event("jumped_on");
 	}
 
 	standing_on_ = stand_info.collide_with;
