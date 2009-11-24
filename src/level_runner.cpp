cc1958910935c4652a4710b9147bb8fea59f8e76
davewx7 <davewx7@c997a996-8b50-0410-b275-33e14bc6d6d6>
2009-11-24 23:13:16 +0000

    made fading when you die work properly

--- src/level_runner.cpp
+++ src/level_runner.cpp
@@ -230,7 +230,7 @@ bool level_runner::play_cycle()
 			return false;
 		}
 		preload_level(save->get_player_info()->current_level());
-		transition_scene(*lvl_, last_draw_position(), false, fade_scene);
+		transition_scene(*lvl_, last_draw_position(), true, fade_scene);
 		level* new_level = load_level(save->get_player_info()->current_level());
 		sound::play_music(new_level->music());
 		set_scene_title(new_level->title());
