13b22fc19439949be6658733303fef892173f143
davewx7 <davewx7@c997a996-8b50-0410-b275-33e14bc6d6d6>
2010-01-09 04:41:22 +0000

    fixed problem with saving strings with quotes in them

--- src/variant.cpp
+++ src/variant.cpp
@@ -663,11 +663,17 @@ void variant::serialize_to_string(std::string& str) const
 		str += "}";
 		break;
 	}
-	case TYPE_STRING:
-		str += "'";
+	case TYPE_STRING: {
+		const char* delim = "'";
+		if(strchr(string_->str.c_str(), '\'')) {
+			delim = "~";
+		}
+
+		str += delim;
 		str += string_->str;
-		str += "'";
+		str += delim;
 		break;
+	}
 	default:
 		assert(false);
 	}
