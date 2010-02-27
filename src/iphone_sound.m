499daa4a249004d739f7ccb92550c5a9afaec7a6
karmosinroda <karmosinroda@c997a996-8b50-0410-b275-33e14bc6d6d6>
2010-02-27 18:53:48 +0000

    Some fixes to iPhone music fading.

--- src/iphone_sound.m
+++ src/iphone_sound.m
@@ -35,14 +35,15 @@ void iphone_fade_in_music (int duration)
 {
 	if (song == nil || delegate == nil || delegate.fading) return;
 	song.volume = 0.0;
-	delegate.amount = (duration/1000) * fade_interval;
+	delegate.amount = 1.0/((duration/1000.0) * (1.0/fade_interval));
+	//NSLog(@"Starting fade in, volume=%f | amount=%f", song.volume, delegate.amount);
 	[delegate fadeInMusic:nil];
 }
 
 void iphone_fade_out_music (int duration)
 {
 	if (song == nil || delegate == nil || delegate.fading) return;
-	delegate.amount = (duration/1000) * fade_interval;
+	delegate.amount = 1.0/((duration/1000.0) * (1.0/fade_interval));
 	[delegate fadeOutMusic:nil];
 }
 
@@ -102,18 +103,20 @@ void iphone_kill_music ()
 {
 	if (song.volume + amount >= 1.0)
 	{
+		//NSLog(@"Done fading in.");
 		fading = NO;
 		song.volume = 1.0; //fading is done, and the volume should be exactly 1.0
 	} else {
 		fading = YES;
 		song.volume += amount;
+		//NSLog(@"Fading in, volume=%f", song.volume);
 		[self performSelector:@selector(fadeInMusic:) withObject:nil afterDelay:fade_interval];
 	}
 }
 
 - (void) fadeOutMusic:(id)obj
 {
-	if (song.volume + amount <= 0.0)
+	if (song.volume - amount <= 0.0)
 	{
 		fading = NO;
 		song.volume = 0.0; //fading is done, and the volume should be exactly 0.0
