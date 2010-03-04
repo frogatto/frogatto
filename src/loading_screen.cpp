958a4b69b73ccfac9d7e9c4f40715d2f0ca4c3f7
crimson_penguin <crimson_penguin@c997a996-8b50-0410-b275-33e14bc6d6d6>
2010-03-04 05:17:07 +0000

    Update to loader bar which still doesn't work

--- src/loading_screen.cpp
+++ src/loading_screen.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <iostream>
 
 #include "loading_screen.hpp"
 #include "wml_node.hpp"
@@ -10,6 +11,7 @@
 
 loading_screen::loading_screen (int items) : items_(items), status_(0)
 {
+	std::cerr << "*** Init loading screen\n";
 }
 
 void loading_screen::load (wml::const_node_ptr node)
@@ -17,22 +19,27 @@ void loading_screen::load (wml::const_node_ptr node)
 	//custom_object_type::get("frogatto_playable");
 	FOREACH_WML_CHILD(preload_node, node, "preload")
 	{
-		draw(wml::get_str(preload_node, "message"));
+		draw_and_increment(wml::get_str(preload_node, "message"));
 		custom_object_type::get(wml::get_str(preload_node, "name"));
-		increment_status();
 	}
 }
 
 void loading_screen::draw (const std::string& message)
 {
+	std::cerr << "*** Drawing loading screen with message: " << message << "\n";
 	const int bar_width = 100;
 	const int bar_height = 20;
+	
 	graphics::prepare_raster();
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	
 	int bar_origin_x = graphics::screen_width()/2 - bar_width/2;
 	rect bg(graphics::screen_width()/2 - bar_width/2 - 1, graphics::screen_height()/2 - bar_height/2 - 1, bar_width+2, bar_height+2);
 	graphics::draw_rect(bg, graphics::color(96, 96, 96, 255));
-	float amount_done = (float)status_ / (float)items_ * bar_width;
+	float amount_done = (float)status_ / (float)items_;
 	rect bar(graphics::screen_width()/2 - bar_width/2, graphics::screen_height()/2 - bar_height/2, bar_width*amount_done, bar_height);
+	std::cerr << "Drawing loader bar with " << status_ << " of " << items_ << " done - " << bar << "\n";
 	graphics::draw_rect(bar, graphics::color(220, 220, 220, 255));
 	SDL_GL_SwapBuffers();
 }
