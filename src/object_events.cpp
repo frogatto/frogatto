04bf37a86284de7bef5966d52479440e75a36dba
Sirp <Sirp@c997a996-8b50-0410-b275-33e14bc6d6d6>
2010-03-14 06:31:33 +0000

    refactoring of object_events to avoid global variables with complex initialization

--- src/object_events.cpp
+++ src/object_events.cpp
@@ -42,37 +42,43 @@ std::vector<std::string> create_object_event_names()
 	return res;
 }
 
-std::vector<std::string> object_event_names = create_object_event_names();
+std::vector<std::string>& object_event_names() {
+	static std::vector<std::string> event_names = create_object_event_names();
+	return event_names;
+}
 
 std::map<std::string, int> create_object_event_ids()
 {
 	std::map<std::string, int> result;
-	for(int n = 0; n != object_event_names.size(); ++n) {
-		result[object_event_names[n]] = n;
+	for(int n = 0; n != object_event_names().size(); ++n) {
+		result[object_event_names()[n]] = n;
 	}
 
 	return result;
 }
 
-std::map<std::string, int> object_event_ids = create_object_event_ids();
+std::map<std::string, int>& object_event_ids() {
+	static std::map<std::string, int> event_ids = create_object_event_ids();
+	return event_ids;
+}
 
 }
 
 const std::string& get_object_event_str(int id)
 {
-	return object_event_names[id];
+	return object_event_names()[id];
 }
 
 int get_object_event_id(const std::string& str)
 {
-	std::map<std::string, int>::iterator itor = object_event_ids.find(str);
-	if(itor != object_event_ids.end()) {
+	std::map<std::string, int>::iterator itor = object_event_ids().find(str);
+	if(itor != object_event_ids().end()) {
 		return itor->second;
 	}
 
 	//we have to add a new entry for this new string
-	object_event_ids[str] = object_event_names.size();
-	object_event_names.push_back(str);
+	object_event_ids()[str] = object_event_names().size();
+	object_event_names().push_back(str);
 
-	return object_event_names.size()-1;
+	return object_event_names().size()-1;
 }
