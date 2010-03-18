946f5534d1413cb15982ca54ddc38c13c37fa091
Sirp <Sirp@c997a996-8b50-0410-b275-33e14bc6d6d6>
2010-03-18 16:02:42 +0000

    implemented non-threaded version of FML level loading

--- src/load_level_nothread.cpp
+++ src/load_level_nothread.cpp
@@ -1,6 +1,73 @@
+#include "asserts.hpp"
+#include "concurrent_cache.hpp"
 #include "filesystem.hpp"
 #include "level.hpp"
 #include "load_level.hpp"
+#include "preferences.hpp"
+#include "preprocessor.hpp"
+#include "wml_parser.hpp"
+
+namespace {
+typedef concurrent_cache<std::string, wml::const_node_ptr> level_wml_map;
+level_wml_map& wml_cache() {
+	static level_wml_map instance;
+	return instance;
+}
+
+class wml_loader {
+	std::string lvl_;
+public:
+	wml_loader(const std::string& lvl) : lvl_(lvl)
+	{}
+	void operator()() {
+		static const std::string path = preferences::load_compiled() ? "data/compiled/level/" : "data/level/";
+		const std::string filename = path + lvl_;
+		wml::const_node_ptr node(wml::parse_wml(preprocess(sys::read_file(filename))));
+		wml_cache().put(lvl_, node);
+	}
+};
+}
+
+void preload_level_wml(const std::string& lvl)
+{
+	if(lvl == "save.cfg" || lvl == "autosave.cfg") {
+		return;
+	}
+
+	if(wml_cache().count(lvl)) {
+		return;
+	}
+
+	wml_loader loader(lvl);
+	loader();
+}
+
+wml::const_node_ptr load_level_wml(const std::string& lvl)
+{
+	if(lvl == "save.cfg" || lvl == "autosave.cfg") {
+		std::string filename;
+		if(lvl == "save.cfg") {
+			filename = preferences::save_file_path();
+		} else {
+			filename = preferences::auto_save_file_path();
+		}
+
+		return wml::parse_wml(preprocess(sys::read_file(filename)));
+	}
+
+	if(wml_cache().count(lvl)) {
+		return wml_cache().get(lvl);
+	}
+
+	wml_loader loader(lvl);
+	loader();
+	return load_level_wml_nowait(lvl);
+}
+
+wml::const_node_ptr load_level_wml_nowait(const std::string& lvl)
+{
+	return wml_cache().get(lvl);
+}
 
 load_level_manager::load_level_manager()
 {
