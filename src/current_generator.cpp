89110a9fd4d23b82c449839426715fa18217d8ce
Sirp <Sirp@c997a996-8b50-0410-b275-33e14bc6d6d6>
2010-04-06 20:02:01 +0000

    made it so currents work on a gradient basis

--- src/current_generator.cpp
+++ src/current_generator.cpp
@@ -81,13 +81,15 @@ void rect_current_generator::generate(int center_x, int center_y, int target_x, 
 {
 	if(point_in_rect(point(target_x, target_y), rect_)) {
 		if(xvelocity_ > 0 && *velocity_x < xvelocity_) {
-			const int amount = (xvelocity_ - std::max(0, *velocity_x))*strength_/(target_mass*1000);
+			const int distance = rect_.x2() - target_x;
+			const int amount = distance*(xvelocity_ - std::max(0, *velocity_x))*strength_/(target_mass*1000*rect_.w());
 			*velocity_x += amount;
 			if(*velocity_x > xvelocity_) {
 				*velocity_x = xvelocity_;
 			}
 		} else if(xvelocity_ < 0 && *velocity_x > xvelocity_) {
-			const int amount = (xvelocity_ - std::min(0, *velocity_x))*strength_/(target_mass*1000);
+			const int distance = target_x - rect_.x();
+			const int amount = distance*(xvelocity_ - std::min(0, *velocity_x))*strength_/(target_mass*1000*rect_.w());
 			*velocity_x += amount;
 			if(*velocity_x < xvelocity_) {
 				*velocity_x = xvelocity_;
@@ -95,13 +97,15 @@ void rect_current_generator::generate(int center_x, int center_y, int target_x, 
 		}
 
 		if(yvelocity_ > 0 && *velocity_y < yvelocity_) {
-			const int amount = (yvelocity_ - std::max(0, *velocity_y))*strength_/(target_mass*1000);
+			const int distance = rect_.y2() - target_y;
+			const int amount = distance*(yvelocity_ - std::max(0, *velocity_y))*strength_/(target_mass*1000*rect_.w());
 			*velocity_y += amount;
 			if(*velocity_y > yvelocity_) {
 				*velocity_y = yvelocity_;
 			}
 		} else if(yvelocity_ < 0 && *velocity_y > yvelocity_) {
-			const int amount = (yvelocity_ - std::min(0, *velocity_y))*strength_/(target_mass*1000);
+			const int distance = target_y - rect_.y();
+			const int amount = distance*(yvelocity_ - std::min(0, *velocity_y))*strength_/(target_mass*1000*rect_.w());
 			*velocity_y += amount;
 			if(*velocity_y < yvelocity_) {
 				*velocity_y = yvelocity_;
