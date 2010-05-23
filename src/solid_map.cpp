e4b0b1ed14564a84c60affb8890275d2d9e85307
Sirp <Sirp@c997a996-8b50-0410-b275-33e14bc6d6d6>
2010-05-23 06:14:53 +0000

    improved collision detection of rectangular props such as the pedestal

--- src/solid_map.cpp
+++ src/solid_map.cpp
@@ -27,7 +27,7 @@ void solid_map::create_object_solid_maps(wml::const_node_ptr node, std::vector<c
 	area = rect(area.x()*2, area.y()*2, area.w()*2, area.h()*2);
 
 	int legs_height = area.w()/2 + 1;
-	if(wml::get_bool(node, "has_feet", true) == false) {
+	if(wml::get_bool(node, "has_feet", true) == false || node->attr("solid_shape").str() == "rect") {
 		legs_height = 0;
 	}
 
