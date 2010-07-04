f0f625cbf4a54d692b1611e0d54a199802d54a65
shadowmaster <shadowmaster@c997a996-8b50-0410-b275-33e14bc6d6d6>
2010-07-04 06:06:14 +0000

    Don't set a 32x32 window icon on Mac OS X

--- src/main.cpp
+++ src/main.cpp
@@ -205,10 +205,12 @@ extern "C" int main(int argc, char** argv)
 	}
 	
 #else
+#ifndef __APPLE__
 	graphics::surface wm_icon = graphics::surface_cache::get("window-icon.png");
 	if(!wm_icon.null()) {
 		SDL_WM_SetIcon(wm_icon, NULL);
 	}
+#endif
 
 	if (SDL_SetVideoMode(preferences::actual_screen_width(),preferences::actual_screen_height(),0,SDL_OPENGL|SDL_RESIZABLE|(preferences::fullscreen() ? SDL_FULLSCREEN : 0)) == NULL) {
 		std::cerr << "could not set video mode\n";
