93abe62baf2cff81f898f4b1f3dce7e9d6551627
crimson_penguin <crimson_penguin@c997a996-8b50-0410-b275-33e14bc6d6d6>
2010-07-18 18:41:33 +0000

    Untested slider widget, not yet used, but does compile.

---
+++ src/slider.cpp
@@ -0,0 +1,86 @@
+
+/*
+ Copyright (C) 2007 by David White <dave@whitevine.net>
+ Part of the Silver Tree Project
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License version 2 or later.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY.
+ 
+ See the COPYING file for more details.
+ */
+#include "slider.hpp"
+#include "iphone_controls.hpp"
+#include "raster.hpp"
+#include "surface_cache.hpp"
+#include "gui_section.hpp"
+
+namespace gui {
+	
+	
+	
+	slider::slider(int width, boost::function<void (float)> onchange)
+	: width_(width), onchange_(onchange), dragging_(false), position_(0.00),
+	slider_left_(gui_section::get("slider_side_left")),
+	slider_right_(gui_section::get("slider_side_right")),
+	slider_middle_(gui_section::get("slider_side_left")),
+	slider_button_(gui_section::get("slider_button"))
+	{
+		set_dim(width_+slider_left_->width()*2, slider_button_->height());
+	}
+	
+	bool slider::in_button(int xloc, int yloc) const
+	{
+		translate_mouse_coords(&xloc, &yloc);
+		int button_x = x() + slider_left_->width() + position_*width_ - slider_button_->width()/2;
+		return xloc > button_x && xloc < button_x + slider_button_->width() &&
+		yloc > y() && yloc < y() + height();
+	}
+	
+	bool slider::in_slider(int xloc, int yloc) const
+	{
+		translate_mouse_coords(&xloc, &yloc);
+		return xloc > x() && xloc < x() + width() &&
+		yloc > y() && yloc < y() + height();
+	}
+	
+	void slider::handle_draw() const
+	{
+		int slider_y = height()/2 - slider_middle_->height()/2;
+		slider_left_->blit(x(), slider_y);
+		slider_middle_->blit(x()+slider_left_->width(), slider_y, width_, slider_middle_->height());
+		slider_right_->blit(x()+slider_left_->width()+width_, slider_y);
+		slider_button_->blit(x()+slider_left_->width()+position_*width()-slider_button_->width()/2, y());
+	}
+	
+	bool slider::handle_event(const SDL_Event& event, bool claimed)
+	{
+		if(claimed) {
+			dragging_ = false;
+		}
+		
+		if(event.type == SDL_MOUSEMOTION && dragging_) {
+			const SDL_MouseMotionEvent& e = event.motion;
+			int rel_x = e.x - x() - slider_left_->width();
+			if (rel_x < 0) rel_x = 0;
+			if (rel_x > width_) rel_x = width_;
+			float pos = rel_x/width_;
+			if (pos != position_)
+			{
+				position_ = pos;
+				onchange_(pos);
+			}
+		} else if(event.type == SDL_MOUSEBUTTONDOWN) {
+			const SDL_MouseButtonEvent& e = event.button;
+			if(in_button(e.x,e.y)) {
+				dragging_ = true;
+			}
+		} else if(event.type == SDL_MOUSEBUTTONUP && dragging_) {
+			dragging_ = false;
+			claimed = true;
+		}
+		return claimed;
+	}
+	
+}
---
+++ src/slider.hpp
@@ -0,0 +1,51 @@
+
+/*
+ Copyright (C) 2010 by David White <dave@whitevine.net>
+ Part of the Frogatto Project
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License version 2 or later.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY.
+ 
+ See the COPYING file for more details.
+ */
+#ifndef SLIDER_HPP_INCLUDED
+#define SLIDER_HPP_INCLUDED
+
+#include <boost/function.hpp>
+
+#include "texture.hpp"
+#include "widget.hpp"
+#include "gui_section.hpp"
+
+
+namespace gui {
+	
+	//A slider widget. Forwards to a given function whenever its value changes.
+	class slider : public widget
+	{
+	public:
+		slider(int width, boost::function<void (float)> onchange);
+		float position() const {return position_;};
+		void set_position (float position) {position_ = position;};
+		
+	private:
+		bool in_slider(int x, int y) const;
+		bool in_button(int x, int y) const;
+		void handle_draw() const;
+		bool handle_event(const SDL_Event& event, bool claimed);
+		
+		int width_;
+		boost::function<void (float)> onchange_;
+		bool dragging_;
+		float position_;
+		
+		const_gui_section_ptr slider_left_, slider_right_, slider_middle_, slider_button_;
+	};
+	
+	typedef boost::shared_ptr<slider> slider_ptr;
+	
+}
+
+#endif
