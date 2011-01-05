d194a26c6476720c674f0c8879e2430bd375223f
Sirp <Sirp@c997a996-8b50-0410-b275-33e14bc6d6d6>
2011-01-05 23:52:04 +0000

    portability fix to texture_frame_buffer

--- src/texture_frame_buffer.cpp
+++ src/texture_frame_buffer.cpp
@@ -39,7 +39,7 @@ void init()
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE); // automatic mipmap
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width(), height(), 0,
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width(), height(), 0,
              GL_RGBA, GL_UNSIGNED_BYTE, 0);
 	glBindTexture(GL_TEXTURE_2D, 0);
 
