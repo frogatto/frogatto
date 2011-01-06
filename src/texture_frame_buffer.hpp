df83f17ae3969507eac981547472c59b79a8e47a
Sirp <Sirp@c997a996-8b50-0410-b275-33e14bc6d6d6>
2011-01-06 23:59:11 +0000

    fix to crash on bentlemyt's machine

--- src/texture_frame_buffer.hpp
+++ src/texture_frame_buffer.hpp
@@ -3,11 +3,12 @@
 
 namespace texture_frame_buffer {
 
+bool unsupported();
 void init();
 
 void set_as_current_texture();
-inline int width() { return 64; }
-inline int height() { return 64; }
+inline int width() { return 128; }
+inline int height() { return 128; }
 
 struct render_scope {
 	render_scope();
