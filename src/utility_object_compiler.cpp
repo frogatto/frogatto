38046364e9e0ea0135e18fb728335bb72be621f5
Sirp <Sirp@c997a996-8b50-0410-b275-33e14bc6d6d6>
2011-06-15 17:39:02 +0000

    made it so experimental objects don't get compiled

--- src/utility_object_compiler.cpp
+++ src/utility_object_compiler.cpp
@@ -138,6 +138,13 @@ UTILITY(object_compiler)
 	std::vector<const_custom_object_type_ptr> types = custom_object_type::get_all();
 	foreach(const_custom_object_type_ptr type, types) {
 		const std::string* path = custom_object_type::get_object_path(type->id() + ".cfg");
+
+		//skip any experimental stuff so it isn't compiled
+		const std::string Experimental = "experimental";
+		if(std::search(path->begin(), path->end(), Experimental.begin(), Experimental.end()) != path->end()) {
+			continue;
+		}
+
 		std::cerr << "OBJECT: " << type->id() << " -> " << *path << "\n";
 		wml::node_ptr obj_node =  wml::parse_wml_from_file(*path);
 		obj_node = custom_object_type::merge_prototype(obj_node);
