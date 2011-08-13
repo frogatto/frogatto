233062e3e19cfd057155fcf750b1dc4d4139ad91
Richard Kettering <kettering.richard@gmail.com>
2011-08-13 00:14:15 -0500

    Added a script to strip colorsync profiles.

---
+++ utils/strip_profiles.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+# script to remove all colorsync/image-calibration profiles from our PNGs, so they don't screw up our transparency (which is now respected by SDL, and wasn't before 1.2.14).
+
+for i in `find images -name '*.png' -print`; do if sips -g profile $i | grep -iP 'profile: [^<]'; then sips -d profile $i; fi; done
