076bd2ed020cd0731579dde1ea51c4ae211eb35f
David D. Roberts <robertsdavidddr@gmail.com>
2012-02-09 00:10:19 -0800

    Added comment-awareness to preprocessor.

--- src/preprocessor.cpp
+++ src/preprocessor.cpp
@@ -8,14 +8,16 @@
 #include "filesystem.hpp"
 std::string preprocess(const std::string& input){
 	std::string output_string;
-
+	bool in_comment = false;
+	
 	std::string::const_iterator i = input.begin();
 	
 	while(i != input.end()){
-		//std::cerr << "test";
-		if(*i == '@'){
-
-
+		if(*i == '#'){//enter a single-line comment
+			in_comment = true;}
+		if(*i == '\n' and in_comment){
+			in_comment = false;}
+		if(*i == '@' and not in_comment){
 			// process pre-processing directive here. See what comes after the '@' and do something appropriate
 			static const std::string IncludeString = "@include";
 			if(input.end() - i > IncludeString.size() && std::equal(IncludeString.begin(), IncludeString.end(), i)) {
@@ -26,7 +28,7 @@ std::string preprocess(const std::string& input){
 					//test for an argument to @include - e.g. "filename.cfg".  First the open quote:
 					std::string::const_iterator quote = std::find(i, input.end(), '"');
 					if(quote == input.end()) {
-						std::cerr << "we didn't find a opening quote. Syntax error." << std::endl;
+						std::cerr << "We didn't find a opening quote. Syntax error." << std::endl;
 					}
 					if(std::count_if(i, quote, isspace) != quote - i) {
 					// # of whitespaces != number of intervening chars => something else was present.  Syntax Error. 
@@ -36,9 +38,8 @@ std::string preprocess(const std::string& input){
 					//now the closing quote, and use it to find what's inbetween:
 					std::string::const_iterator endQuote = std::find(i, input.end(), '"');
 					if(endQuote == input.end()) {
-						std::cerr << "we didn't find a closing quote. Syntax error." << std::endl;
+						std::cerr << "We didn't find a closing quote. Syntax error." << std::endl;
 					}
-					
 					
 					filename_string = std::string(i, endQuote);
 					
