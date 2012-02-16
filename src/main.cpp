12249794a66f4b63082a8ebd404041bb0b4c1836
David White <davewx7@gmail.com>
2012-02-16 17:01:56 -0600

    made it so functions.cfg is available when preloading in preload.cfg

--- src/main.cpp
+++ src/main.cpp
@@ -410,11 +410,11 @@ extern "C" int main(int argc, char** argv)
 		preloads = wml::parse_wml_from_file("data/preload.cfg");
 		int preload_items = std::distance(preloads->begin_child("preload"), preloads->end_child("preload"));
 		loader.set_number_of_items(preload_items+7); // 7 is the number of items that will be loaded below
-		loader.load(preloads);
-		loader.draw_and_increment(_("Initializing custom object"));
 		custom_object::init();
 		loader.draw_and_increment(_("Initializing custom object functions"));
 		init_custom_object_functions(wml::parse_wml_from_file("data/functions.cfg"));
+		loader.draw_and_increment(_("Initializing textures"));
+		loader.load(preloads);
 		loader.draw_and_increment(_("Initializing tiles"));
 		tile_map::init(wml::parse_wml_from_file("data/tiles.cfg",
 		               wml::schema::get("tiles")));
