a355843a8b8208c105293eac9c0bbb9550fa10f6
David White <davewx7@gmail.com>
2012-04-04 00:17:10 -0500

    fixed bug which darkened levels with lighting settings when saving

--- src/color_utils.cpp
+++ src/color_utils.cpp
@@ -160,7 +160,7 @@ variant color_transform::write() const
 {
 	std::vector<variant> res;
 	for(int n = 0; n != 4; ++n) {
-		res.push_back(variant(n));
+		res.push_back(variant(static_cast<int>(rgba_[n])));
 	}
 
 	return variant(&res);
