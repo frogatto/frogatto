5225d149d4ac76392f284903b693f7014bf2b919
Kristina Simpson <sweet.kristas@gmail.com>
2012-04-17 20:50:22 +1200

    Removed a debug message in filesystem.cpp and added the changes to the current windows build file.

--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -543,9 +543,6 @@ int64_t file_mod_time(const std::string& fname)
 
 		return 0;
 	}
-	if(fname.find("test.cfg") != std::string::npos) {
-		std::cerr << "Found time for " << fname << ": " << static_cast<int64_t>(buf.st_mtime) << std::endl;
-	}
 	return static_cast<int64_t>(buf.st_mtime);
 }
 
