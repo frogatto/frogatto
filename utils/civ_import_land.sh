fe712c2cb4c6e4e8c800eae62f69d34b4a713f35
David D. Roberts <robertsdavidddr@gmail.com>
2012-05-15 12:49:47 -0700

    Cleaned up frogatto files for next CT release.

--- utils/civ_import_land.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#script that replaces the "land" variable of tileciv maps with the contents
-#of a numeric matrix in a plaintext file
-#USAGE: in the frogatto top-level folder, run
-#utils/civ_import_land.sh data/level/civ/<level>.txt
-TILES=`python utils/civ_import_land.py $1`;
-sed -i '/^[[:space:]]*#/!s/land[ ]*=[ ]*\[[0-9,]*\]/'"$TILES"'/' ${1%.*}.cfg;
-
