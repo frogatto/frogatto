4ce2beca45f45ad2190f97b17f606c897d4a97ce
David White <davewx7@gmail.com>
2012-06-09 17:52:14 -0500

    made multi-line text get spaced with scaling according to font size

--- src/graphical_font.cpp
+++ src/graphical_font.cpp
@@ -166,7 +166,7 @@ rect graphical_font::do_draw(int x, int y, const std::string& text, bool draw_te
 	int xpos = x, ypos = y, highest = 0;
 	for(std::string::const_iterator i = text.begin(); i != text.end(); ++i) {
 		if(*i == '\n') {
-			ypos = ypos + highest + 10;
+			ypos = ypos + ((highest+4)*size)/2;
 			xpos = x;
 			highest = 0;
 			continue;
