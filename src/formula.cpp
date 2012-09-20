db03f867b964178f676b725e1dcf5b26b434f56c
Kristina Simpson <sweet.kristas@gmail.com>
2012-09-21 07:19:46 +1200

    Fixed a bug where evaluating [1d2]*2 would cause an assertion.

--- src/formula.cpp
+++ src/formula.cpp
@@ -759,7 +759,6 @@ public:
 			switch(op_) {
 			case OP_ADD: return expression_ptr(new add_integer_operator_expression(left_, v.as_int()));
 			case OP_SUB: return expression_ptr(new sub_integer_operator_expression(left_, v.as_int()));
-			case OP_MUL: return expression_ptr(new mul_integer_operator_expression(left_, v.as_int()));
 			case OP_DIV: if(v.as_int() != 0) { return expression_ptr(new div_integer_operator_expression(left_, v.as_int())); } break;
 			case OP_EQ: return expression_ptr(new eq_integer_operator_expression(left_, v.as_int()));
 			case OP_NEQ: return expression_ptr(new ne_integer_operator_expression(left_, v.as_int()));
