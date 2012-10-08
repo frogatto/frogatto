0d3a24642f172d698bb653b191a6e757c8842d64
Richard Kettering <kettering.richard@gmail.com>
2012-10-08 00:09:54 -0500

    Updates to mac project file.

---
+++ MacOSJet/Frogatto/Frogatto/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Frogatto
+//
+//  Created by Richard Kettering on 9/20/12.
+//  Copyright (c) 2012 Richard Kettering. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MacOSJet/Frogatto/Frogatto/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Frogatto
+//
+//  Created by Richard Kettering on 9/20/12.
+//  Copyright (c) 2012 Richard Kettering. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
