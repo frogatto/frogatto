35dd0522a29a1461cb027345cf538ad2f0b77354
David D. Roberts <robertsdavidddr@gmail.com>
2012-11-16 20:00:11 -0800

    CT: Configured pause_game_dialog to only display sound, volume, and resume.

--- src/pause_game_dialog.cpp
+++ src/pause_game_dialog.cpp
@@ -55,7 +55,7 @@ PAUSE_GAME_RESULT show_pause_game_dialog()
 	widget_ptr t2(new graphical_font_label(_("Sound Volume:"), "door_label", 2));
 	widget_ptr s2(new slider(200, boost::bind(sound::set_sound_volume, _1), sound::get_sound_volume()));
 	
-	const int num_buttons = 2 + show_exit + show_controls + show_button_swap + show_of;
+	const int num_buttons = 1;
 	int window_w, window_h;
 	if(preferences::virtual_screen_height() >= 600) {
 		window_w = button_width + padding*4;
@@ -72,8 +72,8 @@ PAUSE_GAME_RESULT show_pause_game_dialog()
 	
 	widget_ptr b1(new button(widget_ptr(new graphical_font_label(_("Resume"), "door_label", 2)), boost::bind(end_dialog, &d, &result, PAUSE_GAME_CONTINUE), BUTTON_STYLE_NORMAL, BUTTON_SIZE_DOUBLE_RESOLUTION));
 	widget_ptr b2(new button(widget_ptr(new graphical_font_label(_("Controls..."), "door_label", 2)), show_controls_dialog, BUTTON_STYLE_NORMAL, BUTTON_SIZE_DOUBLE_RESOLUTION));
-	widget_ptr b3(new button(widget_ptr(new graphical_font_label(_("Return to Titlescreen"), "door_label", 2)), boost::bind(end_dialog, &d, &result, PAUSE_GAME_GO_TO_TITLESCREEN), BUTTON_STYLE_NORMAL, BUTTON_SIZE_DOUBLE_RESOLUTION));
-	widget_ptr b4(new button(widget_ptr(new graphical_font_label(_("Exit Game"), "door_label", 2)), boost::bind(end_dialog, &d, &result, PAUSE_GAME_QUIT), BUTTON_STYLE_DEFAULT, BUTTON_SIZE_DOUBLE_RESOLUTION));
+	widget_ptr b3(new button(widget_ptr(new graphical_font_label(_("Exit (no save) to Map"), "door_label", 2)), boost::bind(end_dialog, &d, &result, PAUSE_GAME_GO_TO_TITLESCREEN), BUTTON_STYLE_NORMAL, BUTTON_SIZE_DOUBLE_RESOLUTION));
+	widget_ptr b4(new button(widget_ptr(new graphical_font_label(_("Exit (no save) Game"), "door_label", 2)), boost::bind(end_dialog, &d, &result, PAUSE_GAME_QUIT), BUTTON_STYLE_DEFAULT, BUTTON_SIZE_DOUBLE_RESOLUTION));
 	widget_ptr b5(new checkbox(_("Reverse A and B"), preferences::reverse_ab(), boost::bind(preferences::set_reverse_ab, _1), BUTTON_SIZE_DOUBLE_RESOLUTION));
 #ifdef ENABLE_OPENFEINT
 	widget_ptr b6(new button(widget_ptr(new graphical_font_label(_("OpenFeint"), "door_label", 2)), of_dashboard, BUTTON_STYLE_NORMAL, BUTTON_SIZE_DOUBLE_RESOLUTION));
@@ -98,26 +98,26 @@ PAUSE_GAME_RESULT show_pause_game_dialog()
 		d.add_widget(t2);
 		d.set_padding(padding+16);
 		d.add_widget(s2);
-		d.set_padding(padding);
-		if (show_button_swap) d.add_widget(b5);
+		//d.set_padding(padding);
+		//if (show_button_swap) d.add_widget(b5);
 		d.add_widget(b1);
-		if (show_controls) d.add_widget(b2);
+		//if (show_controls) d.add_widget(b2);
 #ifdef ENABLE_OPENFEINT
-		if (show_of) d.add_widget(b6);
+		//if (show_of) d.add_widget(b6);
 #endif
-		d.add_widget(b3);
-		if (show_exit) d.add_widget(b4);
+		//d.add_widget(b3);
+		//if (show_exit) d.add_widget(b4);
 	} else {
 		d.set_padding(padding);
 		d.add_widget(b1);
-		if (show_controls) d.add_widget(b2);
+		//if (show_controls) d.add_widget(b2);
 		d.set_padding(padding-16);
 		d.add_widget(t2, padding*3 + button_width, padding*2);
 		d.set_padding(padding+16);
 		d.add_widget(s2);
 		d.set_padding(padding);
-		d.add_widget(b3);
-		if (show_exit) d.add_widget(b4);
+		//d.add_widget(b3);
+		//if (show_exit) d.add_widget(b4);
 	}
 
 	d.set_on_quit(boost::bind(end_dialog, &d, &result, PAUSE_GAME_QUIT));
