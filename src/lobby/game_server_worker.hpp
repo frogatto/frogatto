6f5465f1d6eb505eec9d199aeba0d0b36122af83
Kristina Simpson <sweet.kristas@gmail.com>
2013-02-06 21:25:47 +1300

    More additions to the lobby server.

--- src/lobby/game_server_worker.hpp
+++ src/lobby/game_server_worker.hpp
@@ -6,14 +6,29 @@
 #include "targetver.h"
 #endif
 
+#include <boost/shared_ptr.hpp>
 #include <cstdint>
 #include <string>
+#include <json_spirit.h>
 
 #include "asserts.hpp"
+#include "queue.hpp"
 #include "shared_data.hpp"
 
 namespace game_server
 {
+	typedef queue::queue<std::string> string_queue;
+	typedef boost::shared_ptr<string_queue> string_queue_ptr;
+	struct message
+	{
+		std::string msg;
+		string_queue_ptr reply;
+	};
+
+	typedef queue::queue<message> message_queue;
+	typedef boost::shared_ptr<message> message_ptr;
+	typedef boost::shared_ptr<message_queue> message_queue_ptr;
+
 	class worker
 	{
 	public:
@@ -26,14 +41,21 @@ namespace game_server
 		void operator()();
 		void abort();
 		void process_game(const json_spirit::mObject& obj);
+		double server_load() const { return server_load_; }
+		message_queue_ptr get_queue() { return q_; }
+		const server_info& get_server_info() const { return si_; }
 
+		static worker& get_server_from_game_type(const std::string& game_type);
+		static void get_server_info(json_spirit::mObject* obj);
 	private:
 		bool running_;
 		shared_data& data_;
+		message_queue_ptr q_;
 		int64_t polling_interval_;
 		std::string server_address_;
 		std::string server_port_;
 		server_info si_;
+		double server_load_;
 	};
 }
 
---
+++ src/lobby/name.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef NAME_HPP_INCLUDED
+#define NAME_HPP_INCLUDED
+
+#include <string>
+
+namespace name
+{
+	std::string generate_bot_name();
+}
+
+#endif
