beadb6e2599f1a3faafb5d948faf7715c23e288c
David White <davewx7@gmail.com>
2013-03-02 12:06:53 -0800

    made it so if we don't have an implementation for memory usage querying on a system we use 32bpp rather than 16bpp pixels

--- src/preferences.cpp
+++ src/preferences.cpp
@@ -952,8 +952,8 @@ namespace preferences {
             use_16bpp_textures_ = mem_info.mem_total_kb < memory_required;
             std::cerr << "USING " << (use_16bpp_textures_ ? 16 : 32) << "bpp TEXTURES BECAUSE SYSTEM HAS " << mem_info.mem_total_kb << "KB AND " << memory_required << "KB REQUIRED FOR 32bpp TEXTURES\n";
         } else {
-            use_16bpp_textures_ = true;
-            std::cerr << "USING 16bpp TEXTURES BECAUSE WE COULD NOT DISCOVER HOW MUCH MEMORY THE SYSTEM HAS\n";
+            //use_16bpp_textures_ = true;
+            std::cerr << "COULD NOT DISCOVER HOW MUCH MEMORY THE SYSTEM HAS, SO NOT USING 16bpp TEXTURES\n";
         }
     }
     
