8d892e8fec792e302376bc539c26996af7f5e0b4
Kristina Simpson <sweet.kristas@gmail.com>
2013-03-21 14:40:45 +1300

    Added license information to the source code. Code is now licensed under GPLv2 terms.

--- src/IMG_savepng.cpp
+++ src/IMG_savepng.cpp
@@ -1,4 +1,20 @@
 /*
+	Copyright (C) 2003-2013 by David White <davewx7@gmail.com>
+	
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*
   Based on zlib license - see http://www.gzip.org/zlib/zlib_license.html
 
   This software is provided 'as-is', without any express or implied
--- src/formula.hpp
+++ src/formula.hpp
@@ -1,16 +1,19 @@
-/* $Id: formula.hpp 24955 2008-03-21 23:11:31Z dragonking $ */
 /*
-   Copyright (C) 2007 - 2008 by David White <dave@whitevine.net>
-   Part of the Silver Tree Project
+	Copyright (C) 2003-2013 by David White <davewx7@gmail.com>
+	
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License version 2 or later.
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-   See the COPYING file for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 #ifndef FORMULA_HPP_INCLUDED
 #define FORMULA_HPP_INCLUDED
 
