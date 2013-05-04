f7049e7da1050a2e6ec45a13a781b9fe49c08fea
Kristina Simpson <sweet.kristas@gmail.com>
2013-05-04 14:04:02 +1200

    Separated the anura engine from frogatto. This project now contains the modules relevant to frogatto. More modules may be separated out as time progresses. There may also be teething problems. The engine is now available from: https://github.com/anura-engine/anura This project should be checked out under <path to anura>/modules/

--- src/globals.h
+++
@@ -1,8 +0,0 @@
-#ifndef GLOBALS_H
-#define GLOBALS_H
-
-/* This will be incremented when things pop up on the screen which the game should pause for,
- and decremented when those things go away (it's an int in case >1 such things happen at once).*/
-extern int pause_stack;
-
-#endif
--- src/iphone_device_info.m
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "iphone_device_info.h"
-
-void iphone_screen_res (int *w, int *h)
-{
-	*w = [UIScreen mainScreen].bounds.size.width;
-	*h = [UIScreen mainScreen].bounds.size.height;
-}
