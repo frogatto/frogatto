0cfcb1b2af0feeaccd4738add253b8bf82f3915b
Kristina Simpson <sweet.kristas@gmail.com>
2013-08-22 08:10:25 +1200

    Bumped version to 1.3.3.

--- src/preferences.cpp
+++ src/preferences.cpp
@@ -108,7 +108,7 @@ WindowsPrefs winPrefs;
 
 namespace preferences {
 	const std::string& version() {
-		static const std::string Version = "1.3.2";
+		static const std::string Version = "1.3.3";
 		return Version;
 	}
 	
